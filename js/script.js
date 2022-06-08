6fd8d303357d324e362d93606ae7fc2fced9ffed
Ancrom <remon90@list.ru>
2022-06-08 18:59:49 +0300

    add blocks

--- js/script.js
+++ js/script.js
@@ -1,1 +1,6 @@
-
+document.querySelectorAll(".questions__title").forEach((item) =>
+  item.addEventListener("click", () => {
+    item.classList.toggle("active");
+    item.parentNode.classList.toggle("active");
+  })
+);
