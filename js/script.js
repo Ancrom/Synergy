d0b41d358daa849026239ea174bf1a9d95120034
Ancrom <remon90@list.ru>
2022-07-03 08:48:15 +0300

    new

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 document.querySelectorAll(".questions__item").forEach((item) =>
   item.addEventListener("click", () => {
-    item.querySelector(".questions__text").classList.toggle("active");
+    item.classList.toggle("active");
   })
 );
 
